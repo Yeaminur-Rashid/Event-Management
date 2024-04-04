fd35f4b359ee6fe844442ffea6842f8f65d92ef9
Yeaminur Rashid <yeaminur@icloud.com>
2024-04-05 00:18:33 +0600

    firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDUSszYJfsPXEVOY4ar0ntnZojWHrQenlo",
-  authDomain: "event-management-ec9ec.firebaseapp.com",
-  projectId: "event-management-ec9ec",
-  storageBucket: "event-management-ec9ec.appspot.com",
-  messagingSenderId: "187287003734",
-  appId: "1:187287003734:web:8e446e2197f9e68ced51f7"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
